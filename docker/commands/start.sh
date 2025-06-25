c1ce10b960d814072f7b05cca0488ad4d66b5a95
Sarah Sturgeon <sarah@sarahsturgeon.com>
2025-06-25 15:37:37 -0700

    Fixes with the start command

--- docker/commands/start.sh
+++ docker/commands/start.sh
@@ -4,21 +4,19 @@ gameversion=$(cat $home/metadata/game_version.txt)
 gmodbranch=$(cat $home/metadata/game_branch.txt)
 containertag=$(cat $home/metadata/container_tag.txt)
 
+pidfile="$home/gmodserver/garrysmod/gmod.pid"
+
 # Don't respond until the server is actually ready
-timeout 60s bash -c "until [ -f '$home/gmodserver/garrysmod/gmod.pid' ]; do sleep 0.25; done"
-if [ ! -f "$server/$pidfile" ]; then
-    printf "Content-Type: application/json\r\n"
-    printf "\r\n"
-    printf "{\n"
-    printf "  \"error\": \"Server did not start in time\"\n"
-    printf "}\n"
+timeout 60s bash -c "until [ -f '$pidfile' ]; do sleep 0.25; done"
+if [ ! -f "$pidfile" ]; then
+    printf "Status: 500 Server did not start in time\r\n"
     exit 1
 fi
 
 printf "Content-Type: application/json\r\n"
 printf "\r\n"
 printf "{\n"
-printf "  \"gameversion\": %s,\n" "${gameversion:-unknown}"
-printf "  \"gmodbranch\": %s,\n" "${gmodbranch:-unknown}"
-printf "  \"containertag\": %s\n" "${containertag:-unknown}"
+printf "  \"gameversion\": \"%s\",\n" "${gameversion:-unknown}"
+printf "  \"gmodbranch\": \"%s\",\n" "${gmodbranch:-unknown}"
+printf "  \"containertag\": \"%s\"\n" "${containertag:-unknown}"
 printf "}\n"
